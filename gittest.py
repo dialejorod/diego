8befa5f30551e3b00dfbc5d3913ebab492407a4a
Diego Rodriguez <dialejorod@gmail.com>
2021-03-31 19:15:16 -0500

    22d

---
+++ gittest.py
@@ -0,0 +1,5 @@
+import os
+#os.system('cmd /c "git clone https://github.com/dialejorod/diego.git')
+#os.system('cmd /c "cd C:\Users\diego.rodriguez\repogit2\diego")
+os.system('cmd /c "git add .' )
+os.system('cmd /c "git commit -a -m "22d"')
